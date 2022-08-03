7c19c33984291462ffd7e026241bd47d88becc5d
Ammar Ratnani <ammrat13@gmail.com>
2022-08-03 01:12:10 -0400

    Add portable size

--- include/pmalloc/config.in.h
+++ include/pmalloc/config.in.h
@@ -2,7 +2,7 @@
 // Copyright (C) 2022  Ammar Ratnani <ammrat13@gmail.com>
 /** \file
  *  \defgroup config CMake Configuration
- *  \ingroup private
+ *  \ingroup public
  *  \brief Header containing variables configured by CMake
  *
  * This project is built using CMake. It provides variables the library needs to
