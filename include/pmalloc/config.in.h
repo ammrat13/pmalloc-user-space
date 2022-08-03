ffd10c41ac7b7010d4fd72f771c86a25b8a11ea9
Ammar Ratnani <ammrat13@gmail.com>
2022-08-03 00:48:32 -0400

    Documented configuration

--- include/pmalloc/config.in.h
+++ include/pmalloc/config.in.h
@@ -1,17 +1,33 @@
 // SPDX-License-Identifier: GPL-2.0
 // Copyright (C) 2022  Ammar Ratnani <ammrat13@gmail.com>
+/** \file
+ *  \defgroup config CMake Configuration
+ *  \ingroup private
+ *  \brief Header containing variables configured by CMake
+ *
+ * This project is built using CMake. It provides variables the library needs to
+ * configure itself, like what platform it's targeting and what threading
+ * libraries are available. This is where those variables are injected.
+ *
+ * @{
+ */
 
 // NOLINT(build/header_guard)
 #ifndef PMALLOC_CONFIG_H_
 #define PMALLOC_CONFIG_H_
 
+/** \brief Defined if the target platform is Unix-like, including `APPLE` */
 #cmakedefine PMALLOC_UNIX
+/** \brief Defined if the target platform is Windows */
 #cmakedefine PMALLOC_WIN32
 
+/** \brief Defined if the functions should be thread-safe */
 #cmakedefine PMALLOC_REENTRANT
 
 #ifdef PMALLOC_REENTRANT
+    /** \brief Defined if the thread library found was `pthreads` */
 #   cmakedefine PMALLOC_REENTRANT_PTHREADS
+    /** \brief Defined if the thread library found was for Windows */
 #   cmakedefine PMALLOC_REENTRANT_WIN32_THREADS
 #endif  // PMALLOC_REENTRANT
 
--- include/pmalloc/internals.h
+++ include/pmalloc/internals.h
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0
 // Copyright (C) 2022  Ammar Ratnani <ammrat13@gmail.com>
+/** \file
+ *  \defgroup private Private Components
+ *  \brief Private internals of `pmalloc`
+ */
 
 #ifndef PMALLOC_INTERNALS_H_
 #define PMALLOC_INTERNALS_H_
