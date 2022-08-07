dbdd1be9802db7f97d2e915e64eed8d294ddd7f0
Ammar Ratnani <ammrat13@gmail.com>
2022-08-07 00:01:43 -0400

    Refine tests and add Valgrind

--- tests/simple/create-and-destroy-pool.c
+++ tests/simple/create-and-destroy-pool.c
@@ -1,7 +1,18 @@
-#include <pmalloc/pmalloc.h>
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (C) 2022  Ammar Ratnani <ammrat13@gmail.com>
+
+#include <assert.h>
+
+#include "pmalloc/pmalloc.h"
+#include "pmalloc/internals.h"
+
 
 int main(void) {
     pmalloc_pool_t *pool = pmalloc_create_pool();
+
+    assert(pool->head == NULL);
+    assert(pool->page_size == PMALLOC_DEFAULT_PAGESIZE);
+
     pmalloc_destroy_pool(pool);
     return 0;
 }
