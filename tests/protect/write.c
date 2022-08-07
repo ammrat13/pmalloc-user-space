d29819e8658d080ab21ad2148688bd5c389f3163
Ammar Ratnani <ammrat13@gmail.com>
2022-08-07 01:43:36 -0400

    Workaround no abort

--- tests/protect/write.c
+++ tests/protect/write.c
@@ -6,13 +6,24 @@
 #include "pmalloc/pmalloc.h"
 #include "pmalloc/internals.h"
 
+#if defined(PMALLOC_LINUX)
+#   include <unistd.h>
+#   include <signal.h>
+    void segv_handler(int signal) {
+        _exit(0);
+    }
+#endif
 
 int main(void) {
+    #if defined(PMALLOC_LINUX)
+        signal(SIGSEGV, segv_handler);
+    #endif
+
     pmalloc_pool_t *pool = pmalloc_create_pool();
     char *x = pmalloc(pool, 1);
 
     pmalloc_protect_pool(pool);
     *x = 'A';
 
-    return 0;
+    return 1;
 }
