aa5aa3c23aeaea5261f0737a25af900a1bba1616
Ammar Ratnani <ammrat13@gmail.com>
2022-08-08 00:00:49 -0400

    Fix RPATH issue on Windows

--- cmake/arch-win32.cmake
+++ cmake/arch-win32.cmake
@@ -9,14 +9,6 @@ cmake_dependent_option(
   OFF)
 
 
-cmake_dependent_option(
-  PMALLOC_AGGRESSIVE_PAGESIZE_CHECKS
-  "DEBUG: Assert that the normal page size is 4K and the huge page size is 2M"
-  OFF "PMALLOC_WIN32; PMALLOC_ROUND_PAGESIZE"
-  OFF
-)
-
-
 # On Windows, static and shared libraries use the same file name. Ninja will
 # also complain that pmalloc.lib is built twice. Avoid this.
 if(BUILD_STATIC_LIBS AND BUILD_SHARED_LIBS)
@@ -28,3 +20,19 @@ endif()
 # this for DLL exports.
 set(PMALLOC_IS_STATIC "${BUILD_STATIC_LIBS}")
 set(PMALLOC_IS_SHARED "${BUILD_SHARED_LIBS}")
+
+# Workaround for Windows not having RPATH. The test executables and the
+# libraries have to be in the same directory for Windows to find them. This is
+# fine since the build is fairly simple.
+if(BUILD_TESTING)
+  set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}")
+  set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}")
+endif()
+
+
+cmake_dependent_option(
+  PMALLOC_AGGRESSIVE_PAGESIZE_CHECKS
+  "DEBUG: Assert that the normal page size is 4K and the huge page size is 2M"
+  OFF "PMALLOC_WIN32; PMALLOC_ROUND_PAGESIZE"
+  OFF
+)
